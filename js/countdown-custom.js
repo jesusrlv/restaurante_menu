61c388ad58113aa89f43b6594b2513f60b7d00b4
jesusrlv <jesusrlvrojo@gmail.com>
2023-01-11 16:14:28 -0600

    begin

---
+++ js/countdown-custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+        $(function () {
+            $('#defaultCountdown').countdown({until: new Date(2022, 4, 20, 8)}); // year, month, date, hour
+        });
+});		
+
